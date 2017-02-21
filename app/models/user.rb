f3d0d068700707486c4d135a8be9baecf349c662
Brock Luker <brock@Brocks-iMac.local>
2017-02-21 13:15:31 -0800

    Intial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,29 +15,29 @@ class User < ActiveRecord::Base
 
   REFERRAL_STEPS = [
     {
-      'count' => 5,
-      'html' => 'Shave<br>Cream',
+      'count' => 10,
+      'html' => 'Alone<br>Project',
       'class' => 'two',
       'image' =>  ActionController::Base.helpers.asset_path(
         'refer/cream-tooltip@2x.png')
     },
     {
-      'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
+      'count' => 25,
+      'html' => 'Marshmello<br>Bundle',
       'class' => 'three',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/truman@2x.png')
     },
     {
-      'count' => 25,
-      'html' => 'Winston<br>Shave Set',
+      'count' => 50,
+      'html' => 'One Year<br> Free Classes',
       'class' => 'four',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/winston@2x.png')
     },
     {
-      'count' => 50,
-      'html' => 'One Year<br>Free Blades',
+      'count' => 100,
+      'html' => 'Marshmello<br>Meet & Greet',
       'class' => 'five',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/blade-explain@2x.png')
